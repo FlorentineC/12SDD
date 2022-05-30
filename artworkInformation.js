4684e9a70503351ef061224515444cad6647bfd3
FlorentineC <72718503+FlorentineC@users.noreply.github.com>
2022-05-30 13:02:46 +1000

    Add files via upload

---
+++ artworkInformation.js
@@ -0,0 +1,14 @@
+//Artwork Information
+//exporting arrays from another javascript file
+//source: https://www.tutorialrepublic.com/faq/how-to-include-a-javascript-file-in-another-javascript-file.php
+export const artworklist = [
+    ['bus_2.glb', 1.8,1.6,4.3,'Bus Scene', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'],
+    ['cafe.glb', 1.8,1.6,4.3,'title','description'],
+    ['cafe2.glb',3.3,1.7,-3.2,'Cafe Scene','this is a description'],
+    //Vector3 {x: 3.36809352955974, y: 1.6933962084160634, z: -3.1746425401074547}
+    ['chocolates.glb',0,0,0,''],
+    ['balcony.glb', 0,0,0,''],
+    ['balcony2.glb', 1.8, 1.6, 4.3,
+    'Balcony Scene','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'],
+    ['classroom.glb',1.8,1.6,4.3,'classroom','description']
+];
